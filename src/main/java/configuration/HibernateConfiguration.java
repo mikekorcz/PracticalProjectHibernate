a9223941ea26de686ea3c25f100425f43347b143
Michal <mkorcz01@gmail.com>
2020-08-26 20:12:46 +0200

    Added HibernateConfiguration

--- src/main/java/configuration/HibernateConfiguration.java
+++ src/main/java/configuration/HibernateConfiguration.java
@@ -1,4 +1,20 @@
 package configuration;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class HibernateConfiguration {
+    private static SessionFactory sessionFactory;
+
+    public HibernateConfiguration() {
+    }
+
+    public static SessionFactory getSessionFactory() {
+        Configuration configuration = new Configuration();
+        configuration.configure("hibernate.cfg.xml");
+
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        HibernateConfiguration.sessionFactory = sessionFactory;
+        return sessionFactory;
+    }
 }

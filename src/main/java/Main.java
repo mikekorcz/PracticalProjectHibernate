6234c2fb3e29127bf11c440f17148d9ffcf9a382
Michal <mkorcz01@gmail.com>
2020-08-26 21:06:31 +0200

    Updated Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,27 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        SessionFactory sessionFactory = HibernateConfiguration.getSessionFactory();
-        Session openSession = sessionFactory.openSession();
-
-
-        try {
-            MovieEntity starWars = new MovieEntity("Star Wars", "George Lucas", "USA", 1977);
-
-            openSession.beginTransaction();
-
-            openSession.save(starWars);
-
-            openSession.getTransaction().commit();
-
-        } finally {
-            openSession.close();
-            HibernateConfiguration.closeSessionFactory();
-        }
-
-//        MovieEntity movieEntity = new MovieEntity();
-//        movieEntity.setTitle("Star Wars");
-//        Long title = (Long) session.save(movieEntity);
+        SaveTittle.saveTittle();
 
     }
 }

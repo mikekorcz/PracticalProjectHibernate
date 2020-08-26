c12575076a0ce474ac3c5ddbc24d408fcbc0e077
Michal <mkorcz01@gmail.com>
2020-08-26 21:51:44 +0200

    Updated SaveTittle.java according to table on slack

--- src/main/java/SaveTittle.java
+++ src/main/java/SaveTittle.java
@@ -11,7 +11,9 @@ public class SaveTittle {
         Session openSession = sessionFactory.openSession();
 
         try {
-            MovieEntity starWars = new MovieEntity("Star Wars", "George Lucas", "USA", 1977);
+//            MovieEntity starWars = new MovieEntity("Star Wars", "George Lucas", "USA", 1977);
+            MovieEntity starWars = new MovieEntity("Top Gun", "Tony Scott", "Sensacyjny, Romans", 1986, "Tom Cruise, Kelly McGillis, Val Kilmer", "Rewelacyjny film!, Emocjonujące walki w powietrzu.");
+
 
             openSession.beginTransaction();
 

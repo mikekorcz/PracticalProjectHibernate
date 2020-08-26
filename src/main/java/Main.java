032deb39ab5edd811df2e3ec5b33dbf4ab39c3e8
Michal <mkorcz01@gmail.com>
2020-08-26 21:11:07 +0200

    Updated Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,3 @@
-import entity.MovieEntity;
-import configuration.HibernateConfiguration;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-
 public class Main {
 
     public static void main(String[] args) {

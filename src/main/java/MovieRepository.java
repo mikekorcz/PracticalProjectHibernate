2bd6c289eb965524e0ddc728a5668570fe9633b1
Michal <mkorcz01@gmail.com>
2020-08-30 13:44:58 +0200

    Revert "Trimming texts"

--- src/main/java/MovieRepository.java
+++ src/main/java/MovieRepository.java
@@ -11,9 +11,7 @@ import java.util.List;
 public class MovieRepository {
     private SessionFactory sessionFactory = HibernateConfiguration.getSessionFactory();
     private Session openSession = sessionFactory.openSession();
-public void saveRev(Review review){
 
-}
     public void save(Movie movie) {
         Session openSession = null;
         try {

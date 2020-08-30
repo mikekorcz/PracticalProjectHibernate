4ec268a3f2c149ad736c634a6ca1ef7615f8e246
Michal <mkorcz01@gmail.com>
2020-08-30 09:26:33 +0200

    Updated saveTittle method

--- src/main/java/MovieRepository.java
+++ src/main/java/MovieRepository.java
@@ -13,32 +13,28 @@ public class MovieRepository {
     private static SessionFactory sessionFactory = HibernateConfiguration.getSessionFactory();
     private static Session openSession = sessionFactory.openSession();
 
-    public static void saveTittle() {
+    public static void saveTittle(String title, String director, String filmGenre, Integer productionYear, String starsActorsAndActresses, String reviews) {
         try {
-            MovieEntity topGun = new MovieEntity("Top Gun", "Tony Scott", "Sensacyjny, Romans", 1986, "Tom Cruise, Kelly McGillis, Val Kilmer", "Rewelacyjny film!, Emocjonujące walki w powietrzu.");
-            MovieEntity goldenEye = new MovieEntity("Golden Eye", "Martin Campbell", "Sensacyjny", 1995, "Pierce Brosnan, Sean Bean, Izabella Scorupco", "Najsłabszy z Bondów");
-            MovieEntity mammaMia = new MovieEntity("Mamma Mia!", "Phyllida Lloyd", "Musical, Komedia Romantyczna", 2008, "Meryl Streep, Pierce Brosnan, Colin Firth", "Aż chce się tańczyć!, Mamma Mia! Here I go again!");
-            MovieEntity paniDoubtfire = new MovieEntity("Pani Doubtfire", "Chris Columbus", "Dramat, Komedia", 1993, "Robin Williams, Sally Field, Pierce Brosnan", "Hahahahha!");
-            MovieEntity godFather = new MovieEntity("Ojciec Chrzestny", "Francis Ford Coppola", "Dramat, Gangsterski", 1972, "Marlon Brando, Al Pacino, James Caan", "Strzelaniny cygara i głaskanie kotów, RIP Marlon!");
-            MovieEntity forestGump = new MovieEntity("Forrest Gump", "Robert Zemeckis", "Dramat, Komedia", 1994, "Tom Hanks, Robin Wright, Sally Field", "Tom Hanks, Tom Sizemore, Edward Burns");
-            MovieEntity savingPrivateRyan = new MovieEntity("Szeregowiec Ryan", "Steven Spielberg", "Dramat, Wojenny", 1998, "Tom Hanks, Tom Sizemore, Edward Burns", "Bardzo emocjonujący film, Gloryfikuje wojnę!");
+            openSession = sessionFactory.openSession();
 
+            MovieEntity movieEntity = new MovieEntity();
+            movieEntity.setTitle(title);
+            movieEntity.setDirector(director);
+            movieEntity.setFilmGenre(filmGenre);
+            movieEntity.setProductionYear(productionYear);
+            movieEntity.setStarsActorsAndActresses(starsActorsAndActresses);
+            movieEntity.setReviews(reviews);
 
             openSession.beginTransaction();
-
-            openSession.save(topGun);
-            openSession.save(goldenEye);
-            openSession.save(mammaMia);
-            openSession.save(paniDoubtfire);
-            openSession.save(godFather);
-            openSession.save(forestGump);
-            openSession.save(savingPrivateRyan);
-
+            openSession.save(movieEntity);
             openSession.getTransaction().commit();
 
+        } catch (HibernateException e) {
+            e.printStackTrace();
         } finally {
-            openSession.close();
-            HibernateConfiguration.closeSessionFactory();
+            if (openSession != null) {
+                openSession.close();
+            }
         }
     }
 
